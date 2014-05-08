cc2eae34e516bd84dfc2853bf372482e71c14bf4
Agent 42 <42@enigmabox.net>
2014-05-08 22:23:00 +0200

    only show updates when updates are available - plus display count

--- webinterface/files/root/opt/enigmabox/webinterface/app/templatetags/apptags.py
+++ webinterface/files/root/opt/enigmabox/webinterface/app/templatetags/apptags.py
@@ -38,6 +38,12 @@ def display_version():
     return version
 
 @register.simple_tag
+def updates_count():
+    o = Option()
+    uc = o.get_value('updates_count', '')
+    return uc
+
+@register.simple_tag
 def peer_status(peer_name, sip_peers):
     try:
         ret = re.search(peer_name + '.*5060(.*)', sip_peers).group(1).strip()
@@ -55,4 +61,13 @@ def display_applyconfig_button():
 def if_config_changed(object, __=None):
     o = Option()
     return o.config_changed()
+
+@register.tag
+@condition_tag
+def if_updates(object, __=None):
+    o = Option()
+    uc = o.get_value('updates_count', False)
+    if uc == '0':
+        uc = False
+    return uc
 
