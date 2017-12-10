48bcee7d09e0bb762983fd295ff8e13081c56024
Agent 42 <42@enigmabox.net>
2017-12-10 14:13:05 +0100

    allowed hosts, mistake

--- webinterface/files/root/opt/enigmabox/webinterface/settings.py
+++ webinterface/files/root/opt/enigmabox/webinterface/settings.py
@@ -25,7 +25,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.4/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'box', 'enigma.box', 'box.enigmabox.net', '192.168.100.1', '192.168.101.1']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
